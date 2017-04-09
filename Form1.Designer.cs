1b8ef5a846c3f04ad4d595e59865bd1e4bb7d627
Ronald Johnson <ronjohn83@hotmail.com>
2017-04-09 06:53:59 -0400

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Story_Writer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 60);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(346, 164);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(25, 236);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(346, 28);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 275);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Story_Writer
+{
+    public partial class Form1 : Form
+    {
+
+        Random random = new Random();
+        string[] articals = { "the", "a", "one", "some", "any" };
+        string[] nouns = { "boy", "girl", "dog", "town", "car" };
+        string[] verb = { "drove", "jumped", "ran", "walked", "skipped" };
+        string[] preposition = { "to", "from", "over", "under", "on" };
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            label1.Text = "Click to generate random sentences.";
+   
+        }
+
+        private string Sentence()
+        {
+            List<string> collectionOfArticals = new List<string>();
+
+            int randArticals = random.Next(0, articals.Length);
+            string word = articals[randArticals];
+
+            int randNouns = random.Next(0, nouns.Length);
+            string word2 = nouns[randNouns];
+
+            int randVerbs = random.Next(0, verb.Length);
+            string word3 = verb[randVerbs];
+
+            int randPrep = random.Next(0, preposition.Length);
+            string word4 = preposition[randPrep];
+
+            int randArticals2 = random.Next(0, articals.Length);
+            string word5 = articals[randArticals2];
+
+            int randNouns2 = random.Next(0, nouns.Length);
+            string word6 = nouns[randNouns2];
+
+            return UpperCaseFirst(word) + " " + word2 + " " + word3 + " " + word4 + " " + word5 + " " + word6;
+
+        }
+
+        private void CreateSentences()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                textBox1.AppendText(Sentence() + "." + "\r\n");
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            CreateSentences();
+        }
+
+        private static string UpperCaseFirst(string word)
+        {
+            return char.ToUpper(word[0]) + word.Substring(1);
+        }
+    }
+}
